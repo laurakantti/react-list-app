d5574123a62dca203d944ffac5a1b82c1c678372
Laura Kantti <laura.kantti@chas.se>
2016-04-26 14:53:11 +0200

    Removing router dependency from files

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
 
 import CountryList from '../components/CountryList';
 import CountryDetail from '../components/CountryDetail';
